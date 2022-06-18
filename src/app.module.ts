b661f09964cce138fd3a71e30af2a5e5cfa7f005
Parth Vaghani <parthvaghani14@gmail.com>
2022-06-19 00:21:46 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { ProfileModule } from './profile/profile.module';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+    UserModule,
+    ProfileModule,
+  ],
+  controllers: [
+    AppController
+  ],
+  providers: []
+})
+export class ApplicationModule {
+  constructor(private readonly connection: Connection) {}
+}
